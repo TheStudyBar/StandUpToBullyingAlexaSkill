29005ad8241993ec2d104ecffc06cc5ad4a96497
Brian L <blasker97@gmail.com>
2018-01-09 15:01:14 -0600

    M.V.P. commit

--- alexaBullying/Function.cs
+++ alexaBullying/Function.cs
@@ -17,6 +17,7 @@ namespace alexaBullying
     public class Function
     {
         string phrase = "";
+        bool introStatus = false;
         bool practice1Started = false;
         bool practice2Started = false;
         bool practice3Started = false;
@@ -39,14 +40,51 @@ namespace alexaBullying
                 (innerResponse as PlainTextOutputSpeech).Text = "Welcome to Comforting Comebacks, " +
                     "the number one guide for standing up to bullies effectively. " +
                     "I will simulate a bully and help you practice the best ways to respond. " +
-                    "For example, a bully says Hey, Dummy and your comeback would be, Talk to the hand. " +
                     "Say continue to begin learning.";
 
             }
             else if (input.GetRequestType() == typeof(IntentRequest))
             {
                 var intentRequest = (IntentRequest)input.Request;
-                if (practice1Started == false  && practice2Started == false && practice3Started == false)
+                if (practice1Started == false && practice2Started == false && practice3Started == false && introStatus == false)
+                {
+                    switch (intentRequest.Intent.Name)
+                    {
+                        case "AMAZON.CancelIntent":
+                            log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                            innerResponse = new PlainTextOutputSpeech();
+                            (innerResponse as PlainTextOutputSpeech).Text = "Goodbye!";
+                            response.Response.ShouldEndSession = true;
+                            break;
+                        case "StopIntent":
+                            log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                            innerResponse = new PlainTextOutputSpeech();
+                            (innerResponse as PlainTextOutputSpeech).Text = "Goodbye!";
+                            response.Response.ShouldEndSession = true;
+                            break;
+                        case "AMAZON.HelpIntent":
+                            log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                            innerResponse = new PlainTextOutputSpeech();
+                            (innerResponse as PlainTextOutputSpeech).Text = "add later";
+                            break;
+                        case "ContinueIntent":
+                            log.LogLine($"choose to continue");
+                            innerResponse = new PlainTextOutputSpeech();
+                            (innerResponse as PlainTextOutputSpeech).Text = "The following comebacks work best when you say" +
+                                " them calmy with a hint of sarcasm, then walk away.  When I say hey, dummy, respond immediately " +
+                                "by saying of the three following phrases.  Option 1, talk to the hand.  Option 2, Wow! Now I know why " +
+                                "everyone says that stuff about you.  Option 3, Do you think you could star in the next Mean Girls movie?  " +
+                                "Pick one phrase and say it back now.  If you need to hear them again, say repeat";
+                            introStatus = true;
+                            break;
+                        default:
+                            log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                            innerResponse = new PlainTextOutputSpeech();
+                            (innerResponse as PlainTextOutputSpeech).Text = "Please say continue , or if you dont want to continue just say stop to exit";
+                            break;
+                    }
+                }
+                else if (practice1Started == false && practice2Started == false && practice3Started == false && introStatus == true)
                 {
                     switch (intentRequest.Intent.Name)
                     {
@@ -70,7 +108,7 @@ namespace alexaBullying
                         case "YesIntent":
                             log.LogLine($"Answer Yes");
                             innerResponse = new PlainTextOutputSpeech();
-                            (innerResponse as PlainTextOutputSpeech).Text = "Here we go. ,beep, ,beep, ,beep, Hey, dummy";
+                            (innerResponse as PlainTextOutputSpeech).Text = "Here we go. 3! 2! 1! Hey, dummy";
                             practice1Started = true;
                             break;
                         case "NoIntent":
@@ -79,15 +117,6 @@ namespace alexaBullying
                             (innerResponse as PlainTextOutputSpeech).Text = "If you would like to hear the phrases again say repeat. If you are done" +
                                 " standing up to bullying then say exit or stop";
                             practice1Started = false;
-                            break;
-                        case "ContinueIntent":
-                            log.LogLine($"choose to continue");
-                            innerResponse = new PlainTextOutputSpeech();
-                            (innerResponse as PlainTextOutputSpeech).Text = "The following comebacks work best when you say" +
-                                " them calmy with a hint of sarcasm, then walk away.  When I say hey, dummy, respond immediately " +
-                                "by saying of the three following phrases.  Option 1, talk to the hand.  Option 2, Wow! Now I know why " +
-                                "everyone says that stuff about you.  Option 3, Do you think you could star in the next Mean Girls movie?  " +
-                                "Pick one phrase and say it back now.  If you need to hear them again, say repeat";
                             break;
                         case "RepeatIntent":
                             log.LogLine($"phrases repeated");
@@ -149,7 +178,7 @@ namespace alexaBullying
                             log.LogLine($"phrase 1 choosen");
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Good job. Let's try it again.  Remember" +
-                                " to speak calmly with a hint of sarcasm. ,beep, ,beep, ,beep, Hey, dummy";
+                                " to speak calmly with a hint of sarcasm. 3! 2! 1! Hey, dummy";
                             practice1Started = false;
                             practice2Started = true;
                             break;
@@ -157,7 +186,7 @@ namespace alexaBullying
                             log.LogLine($"phrase 2 choosen");
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Good job. Let's try it again.  Remember" +
-                                " to speak calmly with a hint of sarcasm. ,beep, ,beep, ,beep, Hey, dummy";
+                                " to speak calmly with a hint of sarcasm. 3! 2! 1! Hey, dummy";
                             practice1Started = false;
                             practice2Started = true;
                             break;
@@ -165,7 +194,7 @@ namespace alexaBullying
                             log.LogLine($"phrase 3 choosen");
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Good job. Let's try it again.  Remember" +
-                                " to speak calmly with a hint of sarcasm. ,beep, ,beep, ,beep, Hey, Dummy";
+                                " to speak calmly with a hint of sarcasm. 3! 2! 1! Hey, Dummy";
                             practice1Started = false;
                             practice2Started = true;
                             break;
@@ -202,7 +231,7 @@ namespace alexaBullying
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Great. Let's try one more time." +
                                 "  Remember to make eye contact as you speak, then practice walking away when you're done" +
-                                " speaking. ,beep, ,beep, ,beep, Hey, dummy";
+                                " speaking. 3! 2! 1! Hey, dummy";
                             practice2Started = false;
                             practice3Started = true;
                             break;
@@ -211,7 +240,7 @@ namespace alexaBullying
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Great. Let's try one more time." +
                                 "  Remember to make eye contact as you speak, then practice walking away when you're done" +
-                                " speaking. ,beep, ,beep, ,beep, Hey, dummy";
+                                " speaking. 3! 2! 1! Hey, dummy";
                             practice2Started = false;
                             practice3Started = true;
                             break;
@@ -220,7 +249,7 @@ namespace alexaBullying
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Great. Let's try one more time." +
                                 "  Remember to make eye contact as you speak, then practice walking away when you're done" +
-                                " speaking. ,beep, ,beep, ,beep, Hey, dummy";
+                                " speaking. 3! 2! 1! Hey, dummy";
                             practice2Started = false;
                             practice3Started = true;
                             break;
@@ -275,6 +304,7 @@ namespace alexaBullying
                             innerResponse = new PlainTextOutputSpeech();
                             (innerResponse as PlainTextOutputSpeech).Text = "Okay just say, continue, to start over";
                             practice3Started = false;
+                            introStatus = false;
                             break;
                         case "NoIntent":
                             log.LogLine($"said no");
@@ -282,6 +312,7 @@ namespace alexaBullying
                             (innerResponse as PlainTextOutputSpeech).Text = "Thanks for using Comforting Comebacks. Be sure" +
                                 " to play every week for new phrases and techniques.  Goodbye!";
                             practice3Started = false;
+                            introStatus = false;
                             break;
                         default:
                             log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
